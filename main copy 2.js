c326d199b2404ce2dfb359e789c1582a91f84784
Ricardo <568082704@qq.com>
2019-11-26 20:42:57 +0800

    save

--- main copy 2.js
+++ main copy 2.js
@@ -18,18 +18,34 @@
 //斐波那契数列
 
 
-let a = JSON.parse(window.prompt("请输入数字"))//这是一个字符串
-function fe(n) {
-  if (n===1||n===2) {
-    return 1
-  }else if (n>2) {
-    return  fe(n-2)+fe(n-1)
-  }else{
-    alert("请输入一个正整数")
+// let a = JSON.parse(window.prompt("请输入数字"))//这是一个字符串
+// function fe(n) {
+//   if (n===1||n===2) {
+//     return 1
+//   }else if (n>2) {
+//     return  fe(n-2)+fe(n-1)
+//   }else{
+//     alert("请输入一个正整数")
+//   }
+// }
+
+// console.log(fe(a))
+
+
+//可以简单记一下sort排序的规则，a-b小于0，a在前面(返回值为负)，a-b大于0，b在前面(返回值为正)
+
+
+let str = "hello world"
+function paixu(arr) {
+  let temp =[]
+  for (let i = 0; i < arr.length; i++) {
+    temp.push(str[i].charCodeAt())
   }
+  temp.sort(function (a,b) {
+    return b-a
+  })
+  return temp
 }
 
-console.log(fe(a))
-
-
- 
+let a= paixu(str)
+console.log(a)
