76f0bbc87eeca73e647755720cf10815b4f69b2a
Ricardo <568082704@qq.com>
2020-04-01 20:57:21 +0800

    SAVE

---
+++ xhr.js
@@ -0,0 +1,22 @@
+function ajax(url) {
+  return new Promise((resolve, reject)=>{
+    let xhr = new XMLHttpRequest()
+    xhr.open("get", url)
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState===4) {
+        if (xhr.status>=200 && xhr.status<300) {
+          resolve(
+             JSON.parse(xhr.response)
+          )
+        }else if (xhr.status===404) {
+          reject(new Error("404 not found")) 
+        }
+      }
+    }
+    xhr.send(null)
+  })
+}
+
+let url = "/data.json"
+
+ajax(url).then(data=>console.log(data)).catch(e=>console.error(e))
